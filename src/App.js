ff7b8436a8530cb6d3a19973557c05cf091b5f8f
yubachiri <k.i.moso9532@gmail.com>
2019-10-22 13:40:04 +0900

    各要素の置き場所変えたのとリンク整備

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MuiThemeProvider } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/styles'
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
 
 import './App.css';
 import theme from './theme'
 import HeaderMenu from './HeaderMenu';
 import Top from './Top';
 import Detail from './Detail';
-
-const useStyles = makeStyles({
-  button: {
-    margin: '30px',
-  },
-  links: {
-    margin: '30px',
-  },
-});
-
-function KimonoLinks() {
-  const classes = useStyles();
-
-  const ids = [1, 2, 3];
-  const listItems = ids.map((id) => (
-    <Link to={`/detail/${id}`} key={id}>
-      <Button variant="contained" color="secondary">
-        detail_{id}
-      </Button>
-    </Link>
-  ));
-  return (<div className={classes.links}>{listItems}</div>);
-}
 
 class App extends React.Component {
   render() {
@@ -42,14 +17,6 @@ class App extends React.Component {
           <div className="App">
             <HeaderMenu />
             <Container style={{marginTop: 50}}>
-              <Link to='/'>
-                <Button variant="contained" color="primary">
-                  top
-                </Button>
-              </Link>
-
-              <KimonoLinks />
-
               <Route path='/' exact component={Top} />
               <Route path='/detail/:id' exact component={Detail} />
             </Container>
--- src/Detail.js
+++ src/Detail.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+
 import './App.css';
+import { Button } from '@material-ui/core';
 
 // 詳細画面
 // idもらって出し分けしたい
@@ -12,12 +15,28 @@ class Detail extends React.Component {
   render() {
     return (
       <div className="Detail">
+        <TopButton />
         <h1>Hello, Kimono!</h1>
         <p>this is detail page.</p>
         <p>{this.props.match.params.id}</p>
+        <p>detail contents</p>
       </div>
     );
   }
+}
+
+function TopButton() {
+  const history = useHistory();
+
+  function handleToRootPage() {
+    history.push('/');
+  }
+
+  return (
+    <Button onClick={handleToRootPage} variant="contained" color="primary">
+      Top
+    </Button>
+  );
 }
 
 export default Detail;
