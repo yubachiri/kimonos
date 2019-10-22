6c31ba0fbfe6e4ca47c67839710b464f6bbb9d1e
yubachiri <k.i.moso9532@gmail.com>
2019-10-22 15:33:31 +0900

    axios入れたのとちょっと構成変更

--- src/Top.js
+++ src/Top.js
@@ -4,19 +4,7 @@ import './App.css';
 import { makeStyles } from '@material-ui/styles'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-
-
-// 一覧画面
-function Top() {
-  return (
-    <div className="Top">
-      <h1>Hello, React!</h1>
-      <p>this is top page.</p>
-      <p>list of contents</p>
-      <KimonoLinks />
-    </div>
-  );
-}
+import axios from 'axios';
 
 const useStyles = makeStyles({
   button: {
@@ -26,6 +14,50 @@ const useStyles = makeStyles({
     margin: '30px',
   },
 });
+
+async function fetchKimonos(props) {
+  props.component.setState({ isLoading: true })
+  const url = "https://kimonos.microcms.io/api/v1/kimonos";
+  const resp = await axios.get(url, {
+    headers: { 'X-API-KEY': process.env.REACT_APP_CMS_API_KEY }
+  });
+  console.log(resp.data.contents[0]);
+  props.component.setState({ isLoading: false })
+}
+
+function FetchButton(props) {
+  return (
+    <Button variant="contained" color="secondary" onClick={() => fetchKimonos(props)}>一覧を取得する</Button>
+  );
+}
+
+// 一覧画面
+class Top extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  render() {
+    let contents;
+    if (this.state.isLoading) {
+      contents = <FetchButton component={this} />;
+    } else {
+      contents = <KimonoLinks />;
+    }
+
+    return (
+      <div className="Top">
+        <h1>Hello, React!</h1>
+        <p>this is top page.</p>
+        <p>list of contents</p>
+        {contents}
+      </div>
+    );
+  }
+}
 
 function KimonoLinks() {
   const classes = useStyles();
